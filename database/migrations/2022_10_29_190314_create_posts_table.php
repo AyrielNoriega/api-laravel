16b1d8545c8d38b9d9537d66bf36d31ea92fba91
AyrielNoriega <anoriega2310@gmail.com>
2022-10-30 15:56:01 -0500

    config view

--- database/migrations/2022_10_29_190314_create_posts_table.php
+++ database/migrations/2022_10_29_190314_create_posts_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
 
             $table->timestamps();
             $table->softDeletes();
-            
+
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
