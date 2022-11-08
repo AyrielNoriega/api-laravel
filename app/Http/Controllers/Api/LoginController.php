56463a94d49279fa7e0a3518d7e89ebfd819fd42
AyrielNoriega <anoriega2310@gmail.com>
2022-11-07 20:09:27 -0500

    Acceso

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -13,6 +13,17 @@ class LoginController extends Controller
     public function login(Request $request)
     {
         $this->validateLogin($request);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'token' => $request->user()->createToken($request->name)->plainTextToken,
+                'message' => 'success'
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
     }
 
     public function validateLogin(Request $request)
